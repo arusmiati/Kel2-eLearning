d73ea581fc305651a3baaf95d51aa53e440d189c
arusmiati <mhiaaa31@gmail.com>
2022-03-29 15:42:22 +0800

    Add mapel, detail, pencarian, ujian, docs

--- script.js
+++ script.js
@@ -8,6 +8,44 @@ $(".toggle-password-icon").click(function() {
     }
  });
 
+ $(document).on('click', (".nav-item"), function(){
+     $this.addClass('active').siblings().removeClass('active')
+ })
+
+
+ $(".toggle-filter").click(function(){
+    $(this).toggleClass("fa-angle fa-angle-up");
+
+    let button = document.querySelector('.toggle-filter');
+    let list = document.querySelector('.filter');
+
+    button.addEventListener('click', () => {
+        if(list.style.display === 'none'){
+            dispatchEvent.style.display = 'block';
+            $(this).toggleClass("fa fa-angle-up");
+        }else{
+            list.style.display = 'none';
+        }
+    });
+ });
+
+var tabs = document.querySelectorAll('.tabs-toggle');
+var contents = document.querySelectorAll('.tabs-content');
+
+tabs.forEach((tab, index) => {
+    tab.addEventListener('click', () => {
+        contents.forEach((content) => {
+            content.classList.remove('is-active');
+        });
+        tabs.forEach((tab) => {
+            tab.classList.remove('is-active');
+        });
+
+        contents[index].classList.add('is-active');
+        tabs[index].classList.add('is-active');
+    });
+});
+
  var btnContainer = document.getElementById("navbarNav");
  var btns = btnContainer.getElementsByClassName("btn");
 
